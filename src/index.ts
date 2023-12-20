4642d9931c9f400381164944803618aa33cc3d8a
Ayakura Yuki <YukiAyakura@outlook.com>
2023-12-20 15:30:05 +0800

    returns `Content-MD5` in headers

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export default {
 
 		switch (request.method) {
 			case 'HEAD': {
-				const object = await env.WORKLOAD_BUCKET.get(key);
+				const object = await env.WORKLOAD_BUCKET.head(key);
 				if (!object) {
 					return new Response('Object Not Found', { status: 404 });
 				}
@@ -17,11 +17,24 @@ export default {
 				const headers = new Headers();
 				object.writeHttpMetadata(headers);
 				headers.set('etag', object.httpEtag);
+				headers.set('Content-Length', object.size.toString());
+				if (object.customMetadata) {
+					for (let k in object.customMetadata) {
+						headers.set(k, object.customMetadata[k]);
+					}
+				}
 
 				// get md5 from object checksums
 				let md5 = object.checksums.toJSON().md5;
 				if (md5) {
 					headers.set('X-Object-MD5', md5);
+
+					const bytes = new Uint8Array(md5.length / 2);
+					for (let i = 0; i < md5.length; i += 2) {
+						bytes[i / 2] = parseInt(md5.substring(i, i + 2), 16);
+					}
+					const b64 = btoa(String.fromCharCode.apply(null, bytes as unknown as number[]));
+					headers.set('Content-MD5', b64);
 				}
 
 				return new Response(null, {
